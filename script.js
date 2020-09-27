61b2c7418707da171180c4c5a142ee6c7c3b4a44
amarta <webgraphicshub@gmail.com>
2020-09-27 13:43:44 +0530

    array

--- script.js
+++ script.js
@@ -1,28 +1,5 @@
-// const myFunction = function(){
-    
-// };
+const myArray = [true, null];
 
-// myFunction();
-
-/**
- * Call Back Function
- */
-// let s = 1;
-// let m=0;
-//  setInterval(function(){
-//      console.log(m+":"+s);
-//      s++
-//      if(s==60){
-//          m++
-//          s=0
-//      }
-//  },1000)
-c=1;
-setTimeout(function(){
-const fn = setInterval(function(){
-    console.log("Here is the message number "+c);
-    c++;
-    if (c>5){
-        clearInterval(fn);
-    };
-},2000)},1000)
+myArray.unshift("Hello");
+myArray.unshift(100);
+console.log(myArray);
