34c439cca019443fdb5352041cbe91fa1d96c589
amarta <webgraphicshub@gmail.com>
2020-09-27 13:55:26 +0530

    array

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const myArray = [true, null];
-
-myArray.unshift("Hello");
-myArray.unshift(100);
-console.log(myArray);
+let arrayOfCars = [{carBrand:"BMW", price:2000},{carBrand:"Bugatti", price:5000},{carBrand:"TATA", price:6000}];
+arrayOfCars.push({carBrand:"Ashok Leyland", price:4580});
+console.log(arrayOfCars);
