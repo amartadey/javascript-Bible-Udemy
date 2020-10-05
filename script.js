2c06589b6b803bb1e012f9ce2a52ff5f51f84408
amarta <webgraphicshub@gmail.com>
2020-10-05 13:58:44 +0530

    Ternary Operator

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
+function emptyArray(inputArray){
+    return (inputArray.length>0 ? "Array is not Empty" : "Array is empty");
+}
 
-
-const myObject = {
-    carBrand:"BMW", 
-    price:2000,
-    key1:true,
-    key3:"abc",
-    key10:NaN,
-    test: "Haa"
-};
-
-for (let i in myObject){
-    if(i=="key1"||i=="key3"){
-        console.log(myObject[i]);
-    }
-};
-console.log(i);
+console.log(emptyArray([]))
