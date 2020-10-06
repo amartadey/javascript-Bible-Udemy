2f5be9223c80b442429a3120e118f1c7c2038abb
amarta <webgraphicshub@gmail.com>
2020-10-07 02:16:14 +0530

    ghjkhfjk

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-function emptyArray(inputArray){
-    return (inputArray.length>0 ? "Array is not Empty" : "Array is empty");
-}
+var objs = new Object ({
+    car:"BMW",
+    fuel:true,
+    price:24665
+});
 
-console.log(emptyArray([]))
+console.log(objs);
+
+var func = new Function(){
+    console.log(233);
+};
+func
