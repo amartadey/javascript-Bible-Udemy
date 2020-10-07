5e5b2b78eadf17a0908dc9efca4c5dda7c3fd2e5
amarta <webgraphicshub@gmail.com>
2020-10-08 02:07:57 +0530

    Bind

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const myObject = {
     a:10,
     b:null
-}
-function myFunction(){
+};
+
+function myFunction(a,b){
+    console.log(a+b);
     console.log(this);
 }
+const customFunc = myFunction.bind(myObject);
+const customFunc2 = myFunction.bind(myObject, myObject.a);
 
-myFunction();
-
-myFunction.call(myObject);
+customFunc(1,48);
+customFunc2(3);
