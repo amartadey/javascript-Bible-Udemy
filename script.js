574b07a2569bea9f0eaa1a6597e1d3828d1d7ee7
amarta <webgraphicshub@gmail.com>
2020-10-09 20:28:04 +0530

    Toggle clss in Vanila

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-const myObject = {
-    a:10,
-    b:null
-};
+const menuItems = document.querySelectorAll('.nav-menu');
+console.log(menuItems);
+const LEN = menuItems.length;
 
-function myFunction(a,b){
-    console.log(a+b);
-    console.log(this);
+for (let i=0; i<LEN; i++){
+    menuItems[i].onclick = function(){
+        for(let j=0; j<LEN;j++){
+            menuItems[j].classList.remove('active')
+        }
+        menuItems[i].classList.add('active')
+        console.log(menuItems[i].innerHTML);
+    }
 }
-const customFunc = myFunction.bind(myObject);
-const customFunc2 = myFunction.bind(myObject, myObject.a);
-
-customFunc(1,48);
-customFunc2(3);
