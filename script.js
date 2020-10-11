8fe09f89ca582bd4749c8d2e1b46a20ab8dce21b
amarta <webgraphicshub@gmail.com>
2020-10-11 21:05:51 +0530

    forEach

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-function sum (){
-    console.log(arguments);
-    console.log(Array.from(arguments))
+const myCities = ["London", "New Work","Singapore"];
+
+function arrayInfo(ary){
+    for(let i = 0; i<ary.length;i++){
+        console.log(ary[i]+" is located at the index "+i);
+    };
 }
 
-sum(1,2,3)
+arrayInfo(myCities)
+
+function arrayInfo2(ary){
+    ary.forEach((element,index) => {
+        console.log(element+" is located at the indes "+index);
+    });
+    
+}
+
+arrayInfo2(myCities)
