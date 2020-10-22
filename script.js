d878f872f58026324479357a8e5ed1abcb58f201
amarta <webgraphicshub@gmail.com>
2020-10-22 23:46:23 +0530

    map()

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
-const myCities = ["London", "New York", "Singapore"];
+const myNumbers = [1,3,8];
 
-function arrayInfo(cities){
-    cities.forEach((element,index) =>{
-        console.log(element+' is located at the index '+index)
-    })
-}
-function arrayInfo2(cities){
-    for(let i=0; i<cities.length;i++){
-        console.log(cities[i]+' is located at the index '+ i);
-    }
-}
-arrayInfo(myCities);
-arrayInfo2(myCities);
+let mySquareNumbers = myNumbers.map(element => element*element)
+
+console.log(mySquareNumbers)
+
