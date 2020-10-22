310216d00c7e2410650bf3925d9b8e3c742150b1
amarta <webgraphicshub@gmail.com>
2020-10-23 01:34:02 +0530

    find in object

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-const myArray = [56,[],{},"abc",true,15]
+const posts =[
+    {postId : 1355, commentsQuantity : 5},	
+    {postId : 5131, commentsQuantity : 13},	
+    {postId : 6134, commentsQuantity : 2}	
+];
 
-const result = myArray.find(element =>{
-    return typeof element ==='boolean'
-});
-console.log(result);
+function findSinglePost(urId,post){
+    let res = post.find(element => element.postId==urId);
+    console.log(res)
+}
+
+findSinglePost(1355, posts)
