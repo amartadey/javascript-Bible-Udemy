e887043b826e8b77ab756065cc94b1041fab4301
amarta <webgraphicshub@gmail.com>
2020-10-23 01:11:29 +0530

    map JSON.parse

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ const postsJSON =[
     '{"postId":6134,"commentsQuantity":2}'
 ];
 
-var posts = [];
-for (let i = 0; i < postsJSON.length; i++) {
-    posts.push(JSON.parse(postsJSON[i]));
-}
+// var posts = [];
+// for (let i = 0; i < postsJSON.length; i++) {
+//     posts.push(JSON.parse(postsJSON[i]));
+// }
+
+const posts = postsJSON.map(JSON.parse);
 
 console.log(posts);
 console.log(posts[0].postId);
