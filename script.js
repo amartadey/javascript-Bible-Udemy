7284deff301b602b8a9d577ec3ee06170d729c56
amarta <webgraphicshub@gmail.com>
2020-10-22 23:48:04 +0530

    map

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const myNumbers = [1,3,8];
+const squareNumbers = element => element*element;
 
-let mySquareNumbers = myNumbers.map(element => element*element)
+let mySquareNumbers = myNumbers.map(squareNumbers)
 
 console.log(mySquareNumbers)
 
