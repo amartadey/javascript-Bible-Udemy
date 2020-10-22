63bc71d316dd56ca2819ea417f7bb100c2960b75
amarta <webgraphicshub@gmail.com>
2020-10-22 14:41:04 +0530

    forEach

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-const myArray = [1,true, "abc"];
+const myCities = ["London", "New York", "Singapore"];
 
-myArray.forEach(element => {
-    console.log(element)
-});
-
-var btns = document.querySelectorAll('button');
-btns.forEach(element =>{
-    element.addEventListener('click', function (){
-        btns.forEach(element =>{
-            element.classList.remove('active');
-        })
-        this.classList.add('active')
+function arrayInfo(cities){
+    cities.forEach((element,index) =>{
+        console.log(element+' is located at the index '+index)
     })
-})
+}
+function arrayInfo2(cities){
+    for(let i=0; i<cities.length;i++){
+        console.log(cities[i]+' is located at the index '+ i);
+    }
+}
+arrayInfo(myCities);
+arrayInfo2(myCities);
