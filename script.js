b29169064b91d714450df0e01b8025450871be89
amarta <webgraphicshub@gmail.com>
2020-10-23 00:45:39 +0530

    map

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const myNumbers = [1,3,8];
-const squareNumbers = element => element*element;
+const squareNumbers = (element,index) => {
+    Math.pow(element,2)
+};
 
-let mySquareNumbers = myNumbers.map(squareNumbers)
+// let mySquareNumbers = myNumbers.map(element =>  Math.pow(element, 2))
+let mySquareNumbers = myNumbers.map(Math.pow)
 
 console.log(mySquareNumbers)
 
+
+ 
