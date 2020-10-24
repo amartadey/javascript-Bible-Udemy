caa81b3896a0bcbb8fa1fbba7e50d208e724809d
amarta <webgraphicshub@gmail.com>
2020-10-25 00:13:38 +0530

    sort numbers in an array in ascending order

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-const myNumbers = [5,10,3,15];
+const myNumbers = [10,5,7,12,20];
 
-const sum = arrayOfNumbers => arrayOfNumbers.reduce((accumulator, number) => {console.log ("Accumulator is "+accumulator+". Number is "+ number); return (accumulator+number)}, 0);
 
-console.log(sum(myNumbers))
-// sum(myNumbers)
+
+console.log(myNumbers.sort((a,b)=> a-b))
