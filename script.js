43be25918b86aeae78a62e213ec9827e90eb8bcf
amarta <webgraphicshub@gmail.com>
2020-10-30 17:49:16 +0530

    drf

--- script.js
+++ script.js
@@ -1,21 +1,5 @@
-const personInfo = ({ name, age:personAge, location:{country:origin, city:homeCity }, friendQty=0, recordCreatedAt= new Date().getFullYear()} = person)=>{
-    return {
-        name,
-        personAge,
-        origin,
-        homeCity,
-        friendQty,
-        recordCreatedAt
-    };
+function CivilPlane(){
+
 }
 
-const person = {
-    name: "Alice",
-    age:19,
-    location:{
-        country:"England",
-        city:"London"
-    }
-}
-
-console.log(personInfo(person));
+console.log(CivilPlane.prototype)
